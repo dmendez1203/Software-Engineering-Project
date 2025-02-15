b2c9b5507491d5d1a9eb9640ef0cdd7b11f89207
A-Paige <alexhpaige@gmail.com>
2025-02-14 19:25:09 -0500

    Update dataStorageCenter.java

--- src/APaigeA2/dataStorageCenter.java
+++ src/APaigeA2/dataStorageCenter.java
@@ -5,6 +5,7 @@ public class dataStorageCenter {
 	public void DS(Storage storage ) {
 		StorageKey result = storage.storeData(new StorageRequest());
 		
+		Object key;
 		dataSearch data = storage.retrieveData(key);
 		
 	}
