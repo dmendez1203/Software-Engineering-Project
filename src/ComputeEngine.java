f1752d69d781b20ea0369d712518ec97793082ef
dmendez1203 <157436543+dmendez1203@users.noreply.github.com>
2025-02-18 13:20:58 -0500

    Compute Engine

---
+++ src/ComputeEngine.java
@@ -0,0 +1,12 @@
+import project.annotations.ConceptualAPIPrototype;
+
+public class ComputeEngine {
+
+@ConceptualAPIPrototype	
+public static void anything(Interface1 interface1) {
+int x =3; 	
+interface1.computeDouble(x);
+}	
+	
+	
+}
