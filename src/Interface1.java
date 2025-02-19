38b87240293e5abdd30433f82c62ad4db772dae5
dmendez1203 <157436543+dmendez1203@users.noreply.github.com>
2025-02-19 10:22:53 -0500

    Compute Engine (#26)

---
+++ src/Interface1.java
@@ -0,0 +1,9 @@
+import project.annotations.ConceptualAPI;
+
+@ConceptualAPI
+public interface Interface1 {
+	
+public double computeDouble(int x); // Method should be implemented by the prototype class)	
+	
+
+}
