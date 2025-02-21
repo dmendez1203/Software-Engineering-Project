6b1269aeee4d20a834118ba688cb3cb4dbac87fa
dmendez1203 <157436543+dmendez1203@users.noreply.github.com>
2025-02-20 22:35:08 -0500

    Steps 2-5
    This should contain all work from steps 2-5 from assignment 3.

--- src/ComputeEngine.java
+++ src/ComputeEngine.java
@@ -3,10 +3,17 @@ import project.annotations.ConceptualAPIPrototype;
 public class ComputeEngine {
 
 @ConceptualAPIPrototype	
-public static void anything(Interface1 interface1) {
+public static void Computation(Interface1 interface1) {
 int x =3; 	
 interface1.computeDouble(x);
 }	
-	
+
+@ConceptualAPIPrototype
+public static String getStatus() {
+	//Empty implementation
+	return "";
+}
+
+
 	
 }
