4c4cb2d3efff093783641b0b0fbe20650bbc6cb0
dmendez1203 <157436543+dmendez1203@users.noreply.github.com>
2025-02-21 15:17:18 -0500

    Update ComputeEngine.java

--- src/ComputeEngine.java
+++ src/ComputeEngine.java
@@ -2,18 +2,15 @@ import project.annotations.ConceptualAPIPrototype;
 
 public class ComputeEngine {
 
-@ConceptualAPIPrototype	
-public static void Computation(Interface1 interface1) {
-int x =3; 	
-interface1.computeDouble(x);
-}	
+    @ConceptualAPIPrototype
+    public static void computation(Interface1 interface1) {
+        int x = 3;
+        interface1.computeDouble(x);
+    }
 
-@ConceptualAPIPrototype
-public static String getStatus() {
-	//Empty implementation
-	return "";
-}
-
-
-	
+    @ConceptualAPIPrototype
+    public static String getStatus() {
+        // Empty implementation
+        return "";
+    }
 }
