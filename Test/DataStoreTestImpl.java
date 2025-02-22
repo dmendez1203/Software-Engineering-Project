cabc0739091510b7d08462ea02a876d2f78dfa0a
A-Paige <alexhpaige@gmail.com>
2025-02-21 22:07:50 -0500

    Step 3 and 4b. (#32)

---
+++ Test/DataStoreTestImpl.java
@@ -0,0 +1,19 @@
+import java.util.List;
+
+public class DataStoreTestImpl {
+
+	public inputdata(List<Integer> input) {
+		input = InputConfigTestImpl.getInputData();
+		DataCenter.storeData(input);
+	}
+	
+	public outputdata(String output) {
+		output = DataCenter.retrieveData();
+		OutputConfigTestImpl.writeOutput(output);
+		
+	}
+	
+	public printdata() {
+		System.out.println("current data: " + OutputConfigTestImpl.getOutputData);
+	}
+}
