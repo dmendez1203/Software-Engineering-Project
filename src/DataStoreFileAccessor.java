1b1a8e394872ce055c21ffcff3b2ab12ceebbb5e
A-Paige <alexhpaige@gmail.com>
2025-02-27 20:56:29 -0500

    Create DataStoreFileAccessor.java
    Code for step 3 of assignment 4

---
+++ src/DataStoreFileAccessor.java
@@ -0,0 +1,35 @@
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DataStoreFileAccessor {
+
+	public static void main(String[] args){
+
+		try {
+
+			FileReader fr = new FileReader("Input.txt");
+			FileWriter fw = new FileWriter("Output.txt");
+
+			String str = "";
+			int i;
+
+			while ((i = fr.read()) != -1) {
+				str += (char)i;
+			}
+
+			System.out.println(str);
+			fw.write(str);
+			fr.close();
+			fw.close();
+
+			System.out.println(
+				"File reading and writing both done");
+		}
+		catch (IOException e) {
+
+			System.out.println(
+				"There are some IOException");
+		}
+	}
+}
