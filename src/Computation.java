01a14e27b222b63ef778277747be0b4dea386e72
dmendez1203 <157436543+dmendez1203@users.noreply.github.com>
2025-03-05 10:05:47 -0500

    Assignment5 (#43)
    * Assignment5
    
    * Update TestComputeEngine.java

--- src/Computation.java
+++ src/Computation.java
@@ -1,9 +1,11 @@
-//Consolidate this class with the ComputeEngine class, it seems redundant to have two classes that
-// do the same thing. Talk to Professor at office hours. 
 public class Computation implements Interface1 {
 
 	@Override
 	public double computeDouble(int x) {
+		//Validate that the input is a positive integer
+		if (x < 0) {
+			 throw new IllegalArgumentException("Input must be a positive integer.");
+		}
 		int sum = 0;
 		System.out.print("Multiples of " + x + ": ");
 		for (int i = 1; i <= x; i++) {
