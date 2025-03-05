48f4370446a295547565de8ab85497edcbd148f7
todarom3 <todarom3@newpaltz.edu>
2025-03-05 10:19:37 -0500

    New file.
    Added ExceptionHandler file.

---
+++ src/ExceptionHandler.java
@@ -0,0 +1,8 @@
+public class ExceptionHandler {
+    public static void unknownException() {
+        System.out.println("Unkown Error Occurred.");
+    }
+    public static void handleException(String e) {
+    	System.out.println(e);
+    }
+}
