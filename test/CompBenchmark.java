9a44658d58decb762875c1542028d16dcf1008cf
dmendez1203 <157436543+dmendez1203@users.noreply.github.com>
2025-05-05 11:18:38 -0400

    Update CompBenchmark.java

--- test/CompBenchmark.java
+++ test/CompBenchmark.java
@@ -7,22 +7,22 @@ public class CompBenchmark{
 	
 @Test
 public void testcomputeDouble() {
-	Interface1 InterfaceAlpha = new Computation();
-	Interface1 InterfaceBeta = new Computation2(); 
-	int totalTimeOriginal = timing(InterfaceAlpha);
-	int totalTimeNew = timing(InterfaceBeta); 
+	Interface1 interfaceAlpha = new Computation();
+	Interface1 interfaceBeta = new Computation2(); 
+	int totalTimeOriginal = timing(interfaceAlpha);
+	int totalTimeNew = timing(interfaceBeta); 
 	System.out.println("Original: " + totalTimeOriginal);
 	System.out.println("New: " + totalTimeNew);
 	
 	Assertions.assertTrue( totalTimeNew/ ((double)totalTimeOriginal) <  0.5);
 }
 
-private int timing(Interface1 InterfaceAlpha) {
+private int timing(Interface1 interfaceAlpha) {
 	long startTime = System.currentTimeMillis();
 	for (int i = 0; i < 10; i++) {
-		InterfaceAlpha.computeDouble(100_000_000);
+		interfaceAlpha.computeDouble(100_000_000);
 		for(int j=100_000_000; j<100_000_100; j+=1) {
-			InterfaceAlpha.computeDouble(j);
+			interfaceAlpha.computeDouble(j);
 		} 
 	}
 	long endTime = System.currentTimeMillis();
